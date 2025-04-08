202dfa78e3eba3be1584d48d427bfd80fe2adbf3
naro <hishamsunjeq123@gmail.com>
2025-04-08 12:26:23 +0300

    v0.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import SignIn from "./components/SignIn/SignIn";
+
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/sign-in" replace />} />
         <Route path="/sign-in" element={<SignIn />} />
+        {/* <Route path="*" element={<Navigate to="/sign-in" replace />} /> */}
       </Routes>
     </>
   );
